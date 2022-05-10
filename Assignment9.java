7c46a304d5e21b95beffcfd8ebb391b29d92ec07
Eric Conklin <CONKLIE3@ccf.org>
2022-05-10 08:42:35 -0400

    Add initial file

---
+++ Assignment9.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Assignment9 {
+
+    public static void main(final String[] args) throws IOException {
+        System.out.println("Hello world - this is assignment 9 in Java!");
+        System.out.println("Let's create a graph with 1000 nodes.");
+        System.out.println("That's all. Thanks for joining me!");
+    }
+}
